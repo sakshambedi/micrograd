8ea9a13bde564e91c700916c129d05d798f086af
Saksham Bedi <sakshambedi@icloud.com>
2025-05-20 23:25:18 -0500

    restart grad work

--- grad/ops.py
+++ grad/ops.py
@@ -6,16 +6,3 @@ from enum import Enum, auto
 class Ops(Enum):
     BUFFER = auto()
     DEVICE = auto()
-
-
-# @dataclass
-# class UOp:
-#     op: Ops
-#     dtype: DType  # Store the DType
-#     src: tuple[UOp, ...] | None = None
-#     arg: any | None = None
-#     sz: int | None = None
-
-#     @staticmethod
-#     def new_buffer(device: str, size: int, dtype: DType):
-#         return UOp(Ops.BUFFER, dtype, (UOp(Ops.DEVICE, dtype, arg=device),), sz=size)
