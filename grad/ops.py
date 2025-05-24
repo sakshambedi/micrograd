612e8d44bb8c4f1c0bb284d1606ccd63cdb67d36
Saksham Bedi <sakshambedi@icloud.com>
2025-05-24 12:15:52 -0500

    basic strcutre for grad and computation graph

--- grad/ops.py
+++ grad/ops.py
@@ -1,9 +1,19 @@
 from __future__ import annotations
 
+import operator
+from functools import lru_cache
+
+
+@lru_cache(maxsize=1)
+def get_tensor_class():
+    from grad.tensor import Tensor
+
+    return Tensor
+
 
 class Ops:
     @staticmethod
-    def _to_tensor(x) -> "Tensor":  # type: ignore # noqa : F821
+    def _to_tensor(x):
         from grad.tensor import Tensor
 
         if not isinstance(x, Tensor):
@@ -30,10 +40,28 @@ class Ops:
 
 
 class BinaryOps(Ops):
+    import operator
+
+    @staticmethod
+    def _elementwise_op(a, b, op_func):
+        Tensor = get_tensor_class()
+        result_shape = a.shape
+
+        result = Tensor.zeros(result_shape, dtype=a.dtype)
+        if a.shape == b.shape and a._contiguous and b._contiguous:
+            # Use itertools to apply operation directly on flattened buffers
+            for i, (x, y) in enumerate(zip(a.storage, b.storage)):
+                result.storage[i] = op_func(x, y)
+
+        return result
+
     @staticmethod
     def add(a, b):
-        print("a : ", a)
-        print("b : ", b)
+        return BinaryOps._elementwise_op(a, b, operator.add)
+
+    @staticmethod
+    def sub(a, b):
+        return BinaryOps._elementwise_op(a, b, operator.add)
 
 
 class UnaryOps(Ops):
