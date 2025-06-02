d3a69b34e813e9ed1eb3d4db185fc421b49ee7f4
Saksham Bedi <sakshambedi@icloud.com>
2025-06-02 17:36:41 -0500

    Add roadmap and scaffold for C++ buffer extension

---
+++ kernels/cpu_kernel.cpp
@@ -0,0 +1,10 @@
+#include <Eigen/Dense>
+#include <Eigen/StdVector>
+#include <cstdint>
+#include <memory>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <stdexcept>
+
+namespace py = pybind11;
